bf6c31287b7ee3ed01b43649914fba0c700083b0
Илья Манютин <chem-ixtrems@mail.ru>
2022-09-11 12:27:48 +0300

    Добавьте файлы проекта.

---
+++ 6kyuCountTheSmileyFaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _6kyuCountTheSmileyFaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] smileys = new string[] { ":D", ":~)", ";~D", ":)" };
+
+            var b = smileys.Count(x=> Regex.IsMatch(x, "[:,;]{1}[-,~]{0,1}[D,)]{1}"));
+           ;
+        }
+    }
+}
